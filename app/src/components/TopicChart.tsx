232371f4edd2a67a764080ead3df4bec41156d60
PLejay <pierrelejay1@gmail.com>
2021-03-11 12:27:28 +0000

    feat: Add functions to the topic chart component to make data usable by the chart

--- app/src/components/TopicChart.tsx
+++ app/src/components/TopicChart.tsx
@@ -5,8 +5,41 @@ interface Props {
   chartData: TopicByMonth;
 }
 
+// Get list of the most 'prevalent' topics (likelihood * frequency)
+const listTopTopics = (
+  topicsByMonth: TopicByMonth,
+  amount: number
+): string[] => {
+  const topicList = Object.entries(topicsByMonth.All);
+  topicList.sort((a, b) => b[1] - a[1]);
+
+  let topTopics = [];
+  for (let i = 0; i < amount; i++) {
+    topTopics.push(topicList[i][0]);
+  }
+  return topTopics;
+};
+
+// Get data for each topic in a format easily usable by the chart
+// [['Month', prevalence]...]
+const getTopicData = (topic: string, topicsByMonth: TopicByMonth) => {
+  let topicData = [];
+  for (const month in topicsByMonth) {
+    if (month === 'All') continue;
+
+    // Declaring types for Typescript's benefit - see https://stackoverflow.com/questions/59233965/typescript-element-implicitly-has-type-any-with-for-in-loops
+    const monthObject = topicsByMonth[month as keyof typeof topicsByMonth];
+    topicData.push([month, monthObject[topic as keyof typeof month]]);
+  }
+
+  return topicData;
+};
+
 function TopicChart(props: Props) {
   const { chartData } = props;
+
+  const topTopics = listTopTopics(chartData, 5);
+  console.log(getTopicData(topTopics[0], chartData));
 
   return <div></div>;
 }
